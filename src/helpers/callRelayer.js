2dc9d5ca45f5532d9391646e312b751985a28762
oaswap <oaswap@protonmail.com>
2022-01-27 17:13:06 -0600

    Add fetch function to call relay url

--- src/helpers/callRelayer.js
+++ src/helpers/callRelayer.js
@@ -1,3 +1,14 @@
-export async function callRelayer() {
+export async function callRelayer(mainnetProvider, walletAddress) {
+  console.log("ajksdfsad");
+  if (!walletAddress) throw new Error(`Address cannot be empty`);
+  const url = process.env.REACT_APP_RELAYER_URL;
+  const request = { address: walletAddress };
+  console.log(request);
 
+  return fetch(url, {
+    method: "POST",
+    mode: 'cors',
+    body: JSON.stringify(request),
+    headers: { "Content-Type": "application/json" },
+  });
 }
