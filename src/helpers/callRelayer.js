264ed56c0747d4b5eb2f32ed2be7b478222b50c9
oaswap <oaswap@protonmail.com>
2022-01-27 22:46:48 -0600

    Add cors mode to fetch to allow json content type in header, add handleError function to fetch

--- src/helpers/callRelayer.js
+++ src/helpers/callRelayer.js
@@ -1,14 +1,22 @@
 export async function callRelayer(mainnetProvider, walletAddress) {
-  console.log("ajksdfsad");
   if (!walletAddress) throw new Error(`Address cannot be empty`);
   const url = process.env.REACT_APP_RELAYER_URL;
   const request = { address: walletAddress };
-  console.log(request);
 
-  return fetch(url, {
+  const handleErrors = response => {
+    console.log("response", response);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  };
+
+  fetch(url, {
     method: "POST",
-    mode: 'cors',
+    mode: "cors",
     body: JSON.stringify(request),
     headers: { "Content-Type": "application/json" },
-  });
+  })
+    .then(handleErrors)
+    .catch(error => console.log(error));
 }
